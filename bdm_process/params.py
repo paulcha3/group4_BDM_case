d9bebaff262bbc0e5a46bd6220aba2ac072a370b
Alexandre-sus <alexandre.zente@gmail.com>
2026-02-11 19:50:51 +0100

    Create params.py

---
+++ bdm_process/params.py
@@ -0,0 +1,9 @@
+import os
+
+# Identifiants BigQuery
+PROJECT_ID = "projectbdm-487109"
+DATASET_ID = "patek_data"
+TABLE_ID = "patek"
+
+brand = "Patek Philippe"
+KEY_PATH = "/home/alexandre/projects/project-toto/credentials/credentials.json"
